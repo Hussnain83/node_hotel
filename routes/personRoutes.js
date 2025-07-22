d55af69b5e9096c40ece2d4f4d56c4efed18d1ec
Hussnain83 <hussnaindogar2006@gmail.com>
2025-07-22 17:22:18 +0500

    Added JWT auth and updated routes

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,21 +1,88 @@
 const express = require("express");
 const router = express.Router();
 const Person = require("./../models/Person");
+const {jwtAuthMiddleware, generateToken} = require("./../jwt");
 
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const data = req.body;
     const newPerson = new Person(data);
     const response = await newPerson.save();
     console.log("data saved");
-    res.status(200).json(response);
+
+    const payload = {
+      id: response.id,
+      username: response.username,
+    }
+    console.log(JSON.stringify(payload));
+
+    const token = generateToken(payload);
+    console.log("Token is : ", token);
+
+
+
+    res.status(200).json({response: response, token: token});
+
+
+    
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "internal server error" });
   }
 });
 
-router.get("/", async (req, res) => {
+// Login Routes
+router.post("/login", async(req,res)=>{
+  try{
+    const {username,password} = req.body;
+
+    const user = await Person.findOne({username: username});
+    
+    if(!user || !(await user.comparePassword(password))){
+      return res.status(401).json({error: "Invalid username or password"});
+    }
+    // generate token
+    const payload = {
+      id: user.id,
+      username: user.username
+    }
+    const token = generateToken(payload);
+    res.json({token})
+
+
+  }
+  catch(err){
+    console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+
+
+  }
+})
+
+
+
+// Profile routes
+router.get("/profile", jwtAuthMiddleware, async(req, res) => {
+  try{
+    const userData = req.user;
+    console.log("user Date: ", userData);
+    
+    const userId = userData.id;
+    const user = await Person.findById(userId);
+    res.status(200).json({user});
+
+  }
+  catch(err){
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+
+
+  }
+})
+
+
+
+router.get("/",jwtAuthMiddleware, async (req, res) => {
   try {
     const data = await Person.find();
     console.log("data fetched");
@@ -25,6 +92,8 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+
 
 router.get("/:workType", async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -2,15 +2,32 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 require("dotenv").config();
+
+const passport = require("./auth");
+
+
+
 const PORT = process.env.PORT || 3000;
 
+// Middleware Function
+
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] Request Made to: ${req.originalUrl}`);
+  next();
+}
+
+app.use(logRequest); // Middle ware for all
 
 
+app.use(passport.initialize());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+
+const localAuthMiddleware = passport.authenticate("local", {session: false});
+
+app.get("/" ,(req, res) => {
   res.send("Welcome to our Hotel!");
 });
 
@@ -18,7 +35,9 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 const personRoutes = require("./routes/personRoutes");
 
 
-app.use("/person", personRoutes);
+//localAuthMiddleware, at between /person
+app.use("/person",
+  personRoutes);
 app.use("/menu", menuItemRoutes);
 
 
